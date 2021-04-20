80838a003b80375553541fe99fa6533032adb6d4
nanov <nanovche1@gmail.com>
2021-04-20 13:08:40 +0300

    created custom print stream

--- src/main/java/com/egtinteractive/app/CustomPrintStream.java
+++ src/main/java/com/egtinteractive/app/CustomPrintStream.java
@@ -1,10 +1,19 @@
-//package com.egtinteractive.app;
-//
-//public class CustomPrintStream {
-//
-//  public static void print(String message){
-//    System.out.println(message);
-//    System.setOut();
-//  }
-//
-//}
+package com.egtinteractive.app;
+
+import java.util.List;
+
+public class CustomPrintStream {
+
+    private static List<String> messages;
+    private static int index = 0;
+
+    public static void print(String message){
+        System.out.println(message);
+        messages.add(message);
+    }
+
+    public static String getMessage() {
+        return messages.get(index++);
+    }
+
+}
